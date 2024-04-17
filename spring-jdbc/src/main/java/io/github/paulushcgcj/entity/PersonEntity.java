e08f2b2c1e6948c1f606e6c2c462a39aa0521a52
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-04-17 14:03:05 -0700

    feat: jdbc with encrypted field

--- spring-jdbc/src/main/java/io/github/paulushcgcj/entity/PersonEntity.java
+++ spring-jdbc/src/main/java/io/github/paulushcgcj/entity/PersonEntity.java
@@ -11,6 +11,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import org.hibernate.annotations.ColumnTransformer;
 
 @Entity
 @Table(name = "person", schema = "secure")
@@ -26,15 +27,31 @@ public class PersonEntity {
     private Long id;
 
     @Column(name = "first_name", length = 50)
+    @ColumnTransformer(
+        read = "pgp_sym_decrypt(first_name, current_setting('cryptic.key'))",
+        write = "pgp_sym_encrypt(?, current_setting('cryptic.key'))"
+    )
     private String firstName;
 
     @Column(name = "last_name", length = 50)
+    @ColumnTransformer(
+        read = "pgp_sym_decrypt(last_name, current_setting('cryptic.key'))",
+        write = "pgp_sym_encrypt(?, current_setting('cryptic.key'))"
+    )
     private String lastName;
 
     @Column(name = "email", length = 50)
+    @ColumnTransformer(
+        read = "pgp_sym_decrypt(email, current_setting('cryptic.key'))",
+        write = "pgp_sym_encrypt(?, current_setting('cryptic.key'))"
+    )
     private String email;
 
     @Column(name = "gender", length = 50)
+    @ColumnTransformer(
+        read = "pgp_sym_decrypt(gender, current_setting('cryptic.key'))",
+        write = "pgp_sym_encrypt(?, current_setting('cryptic.key'))"
+    )
     private String gender;
 
 }
